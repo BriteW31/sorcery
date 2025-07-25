3f03c2f1c983880d478f781e17aa3bab17822ae9
Bright <168295604+BriteW31@users.noreply.github.com>
2025-07-24 22:10:58 -0400

    Update ability.cc

--- ability.cc
+++ ability.cc
@@ -1,7 +1,34 @@
 #include "ability.h"
+#include <iostream>
 
 ActivatedAbility::ActivatedAbility(int cost) : cost{cost} {}
 
 int ActivatedAbility::getCost() const {
     return cost;
+}
+
+void ActivatedAbility::addExtraCost(int extra) {
+    cost += extra;
+}
+
+void ActivatedAbility::execute(Game &game, int targetPlayer, int targetCard) {
+    std::cout << "ActivatedAbility::execute not implemented.\n";
+}
+
+AbilityType ActivatedAbility::getType() const {
+    return AbilityType::Activated;
+}
+
+TriggeredAbility::TriggeredAbility(std::string key) : triggerKey{std::move(key)} {}
+
+const std::string &TriggeredAbility::key() const {
+    return triggerKey;
+}
+
+AbilityType TriggeredAbility::getType() const {
+    return AbilityType::Triggered;
+}
+
+void TriggeredAbility::execute(Game &game, int targetPlayer, int targetCard) {
+    std::cout << "TriggeredAbility::execute not implemented.\n";
 }
